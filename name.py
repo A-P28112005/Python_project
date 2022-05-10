f3ab4795eebc09724354fa53ec2ecbc810696df7
A-P28112005 <aaronpaul28112005@gmail.com>
2022-05-10 20:33:17 +0200

    "Second commit"

--- name.py
+++ name.py
@@ -7,6 +7,20 @@ def collide(x1, x2, y1, y2, w1, w2, h1, h2):
     else:
         return False
 
+def die(screen, score):
+    f=pygame.font.SysFont('times bold', 30);t=f.render('Your score was: '+str(score), True, (255, 255, 255));screen.blit(t, (10, 270));pygame.display.update();pygame.time.wait(2000);sys.exit(0)
+xs = [290, 290, 290, 290, 290];ys = [290, 270, 250, 230, 210];dirs = 0; score = 0;
+applepos = (random.randint(0, 590), random.randint(0, 590));
+pygame.init();
+
+s=pygame.display.set_mode((600, 600));
+pygame.display.set_caption('Snake Game');
+appleimage = pygame.Surface((16, 16));
+appleimage.fill((127, 255, 0));
+img = pygame.Surface((20, 20));
+img.fill((255, 0, 0));
+f = pygame.font.SysFont('times bold', 30);
+clock = pygame.time.Clock()
 
 
 
