57c5322f3a3d2fdd93fb24e2548156ea90c62b71
A-P28112005 <aaronpaul28112005@gmail.com>
2022-05-10 16:59:51 +0200

    "Created and instantiated class"

--- name.py
+++ name.py
@@ -1,1 +1,15 @@
-print('Hello')
+greeting_message = "Greetings and welcome to my program"
+class Ephesians:
+    def __init__(self, book = "Ephesians", chapter = range(1, 7), verse = range(1, 34)):
+        self.book = book
+        self.chapter = chapter
+        self.verse = verse
+    def __repr__(self):
+        return self.book
+
+ephesians = Ephesians()
+
+
+
+
+
